feb52e00b9f1019231c394ccab5ec3584142a18a
Paulina Sikora <paaulinaaas@gmail.com>
2017-11-10 11:42:04 +0100

    done

---
+++ server/controllers/note.controller.js
@@ -0,0 +1,36 @@
+import Note from '../models/note';
+import Lane from '../models/lane';
+
+export function addNote(req, res) {
+  if (!req.body.task) {
+    res.status(403).end();
+  }
+
+  const newNote = new Note(req.body);
+
+  newNote.id = uuid();
+  newNote.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    Lane.findOne({id: req.params.laneId})
+      .then(lane => {
+        lane.notes.push(saved);
+        return lane.save()
+      })
+      .then(() => {
+        res.json(saved);
+      });
+  });
+}
+
+export function deleteNote(req, res) {
+  Lane.findById(req.params.laneId).exec()
+  .then(foundLane => {
+    if (foundLane) {
+      const index = foundLane.notes.findIndex(n => String(n._id) === req.params.noteId);
+      if (index >= 0) {
+        foundLane.notes.splice(index, 1);
+      }
+      return foundLane.save();
+    }
